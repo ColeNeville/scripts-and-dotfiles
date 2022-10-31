64cf034a0f38f6acd8e5e0e6602d4302ee5700c4
Cole Neville <colewneville2@gmail.com>
2022-10-31 16:56:41 -0600

    Moving my laptop to Pop!_OS

--- scripts/garuda.sh
+++ scripts/garuda.sh
@@ -21,17 +21,8 @@ if [ ! -d /home/cole/.oh-my-zsh ]; then
 fi
 
 
-# Remove snapd
-sudo rm -rf /var/cache/snapd
-snap remove --purge firefox
-sudo apt-get autoremove --purge -y snapd gnome-software-plugin-snap
-sudo apt-mark hold snapd
-rm -rf ~/snap
-
-
 # Install flatpak and flathub
 sudo apt-get install -y flatpak
-sudo apt-get install gnome-software gnome-software-plugin-flatpak
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 flatpak install flathub com.brave.Browser
@@ -39,26 +30,13 @@ flatpak install flathub com.valvesoftware.Steam
 flatpak install flathub com.spotify.Client
 
 
-# Install Pop!_OS shell
-sudo apt-get install -y gnome-shell-extensions gnome-shell-extensions-manager node-typescript make
-
-git clone https://github.com/pop-os/shell.git pop-os-shell
-cd pop-os-shell
-git checkout master_jammy
-make local-install
-
-
 # Install stow and install dotfiles
-sudo apt-get stow
+sudo apt-get install -y stow
 
 cd dotfiles
-
+s
 stow --target=/home/cole zsh
 
 cd ..
 
-
-
 cd $currentDir
-
-sudo reboot now
